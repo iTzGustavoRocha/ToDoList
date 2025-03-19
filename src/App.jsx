843c79d5fceaf5668f7db0c12b952e18364bf5f6
iTzGustavoRocha <itzgustax@gmail.com>
2025-03-18 21:31:06 -0300

    function signin

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import react from 'react'
+import GlobalStyle from './styles/global'
+import RoutesApp from './routes'
+
+function App() {
+
+  return (
+    <>
+      <RoutesApp />
+      <GlobalStyle />
+    </>
+  );
+};
+
+export default App;
---
+++ src/styles/global.jsx
@@ -0,0 +1,20 @@
+import { createGlobalStyle } from "styled-components";
+
+
+const Global = createGlobalStyle`
+
+    * {
+        margin: 0;
+        padding: 0;
+    }
+
+    body {
+        font-family: 'Roboto', serif;
+        background:rgb(0, 183, 255);
+       
+    }
+
+
+`;
+
+export default Global;
